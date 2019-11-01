d012749bd127837f343477f69dfea8cb932214d2
Papp Imre <imi950127@gmail.com>
2019-11-01 18:37:36 +0100

    added user model and validation

---
+++ routes/vehicles.js
@@ -0,0 +1,4 @@
+// const express = require('express');
+// const bodyParser = require('body-parser').json();
+// const router = express.Router();
+// const { check, validationResult } = require('express-validator');
--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect DB
+connectDB();
 
 app.get('/', (req, res) => {
   res.json({ msg: 'Welcome' });
 });
+
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+// app.use('/api/auth', require('./routes/auth'));
+// app.use('/api/vehicles', require('./routes/vehicles'));
 
 const PORT = process.env.PORT || 5000;
 
