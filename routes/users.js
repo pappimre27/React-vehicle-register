d38f941391cbd16840b0d7e1c9ec34571608af67
Papp Imre <imi950127@gmail.com>
2019-11-01 19:17:44 +0100

    hash password and register route

--- routes/users.js
+++ routes/users.js
@@ -32,49 +32,50 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('passed');
-    // const { name, email, password } = req.body;
+    const { name, email, password } = req.body;
 
-    // try {
-    //   let user = await User.findOne({ email });
+    try {
+      let user = await User.findOne({ email });
 
-    //   if (user) {
-    //     return res.status(400).json({ msg: 'User already exists' });
-    //   }
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists' });
+      }
 
-    //   user = new User({
-    //     name,
-    //     email,
-    //     password
-    //   });
+      user = new User({
+        name,
+        email,
+        password
+      });
 
-    //   const salt = await bcrypt.genSalt(10);
+      const salt = await bcrypt.genSalt(10);
 
-    //   user.password = await bcrypt.hash(password, salt);
+      user.password = await bcrypt.hash(password, salt);
 
-    //   await user.save();
+      await user.save();
 
-    // const payload = {
-    //   user: {
-    //     id: user.id
-    //   }
-    // };
+      res.send('User saved');
 
-    // jwt.sign(
-    //   payload,
-    //   config.get('jwtSecret'),
-    //   {
-    //     expiresIn: 360000
-    //   },
-    //   (err, token) => {
-    //     if (err) throw err;
-    //     res.json({ token });
-    //   }
-    // );
-    // } catch (err) {
-    //   console.error(err.message);
-    //   res.status(500).send('Server error');
-    // }
+      // const payload = {
+      //   user: {
+      //     id: user.id
+      //   }
+      // };
+
+      // jwt.sign(
+      //   payload,
+      //   config.get('jwtSecret'),
+      //   {
+      //     expiresIn: 360000
+      //   },
+      //   (err, token) => {
+      //     if (err) throw err;
+      //     res.json({ token });
+      //   }
+      // );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
   }
 );
 
