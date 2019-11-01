cd2f89a9d6f08d7dfbf133eb909d68d251df6a6d
Papp Imre <imi950127@gmail.com>
2019-11-01 19:30:33 +0100

    creating and responding with json web token

--- routes/users.js
+++ routes/users.js
@@ -53,25 +53,23 @@ router.post(
 
       await user.save();
 
-      res.send('User saved');
+      const payload = {
+        user: {
+          id: user.id // to get specific information in case we need that
+        }
+      };
 
-      // const payload = {
-      //   user: {
-      //     id: user.id
-      //   }
-      // };
-
-      // jwt.sign(
-      //   payload,
-      //   config.get('jwtSecret'),
-      //   {
-      //     expiresIn: 360000
-      //   },
-      //   (err, token) => {
-      //     if (err) throw err;
-      //     res.json({ token });
-      //   }
-      // );
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
