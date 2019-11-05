3c6581e9488108c0944f00cd3b9d79970fb23d32
Papp Imre <imi950127@gmail.com>
2019-11-05 13:31:46 +0100

    redux setup

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,8 @@ import AlertState from './context/alert/AlertState';
 import AuthState from './context/auth/AuthState';
 import setAuthToken from './utils/setAuthToken';
 import PrivateRoute from './components/routing/PrivateRoute';
+import { Provider } from 'react-redux';
+import store from './store';
 import './App.css';
 
 if (localStorage.token) {
@@ -19,26 +21,28 @@ if (localStorage.token) {
 
 const App = () => {
   return (
-    <AuthState>
-      <VehicleState>
-        <AlertState>
-          <Router>
-            <Fragment>
-              <Navbar />
-              <div className='container'>
-                <Alerts />
-                <Switch>
-                  <PrivateRoute exact path='/' component={Home} />
-                  <Route exact path='/about' component={About} />
-                  <Route exact path='/register' component={Register} />
-                  <Route exact path='/login' component={Login} />
-                </Switch>
-              </div>
-            </Fragment>
-          </Router>
-        </AlertState>
-      </VehicleState>
-    </AuthState>
+    <Provider store={store}>
+      <AuthState>
+        <VehicleState>
+          <AlertState>
+            <Router>
+              <Fragment>
+                <Navbar />
+                <div className='container'>
+                  <Alerts />
+                  <Switch>
+                    <PrivateRoute exact path='/' component={Home} />
+                    <Route exact path='/about' component={About} />
+                    <Route exact path='/register' component={Register} />
+                    <Route exact path='/login' component={Login} />
+                  </Switch>
+                </div>
+              </Fragment>
+            </Router>
+          </AlertState>
+        </VehicleState>
+      </AuthState>
+    </Provider>
   );
 };
 
