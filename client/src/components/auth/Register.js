8f6d6de51e04bee0cf9f456f4f825b21b48de25a
Papp Imre <imi950127@gmail.com>
2019-11-12 19:44:29 +0100

    redirecting the user after register to vehicle pages

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { setAlert } from '../../actions/alertAction';
-import { clearErrors, register } from '../../actions/authAction';
-import { connect } from 'react-redux';
+import React, { useState, useEffect } from "react";
+import { setAlert } from "../../actions/alertAction";
+import { clearErrors, register } from "../../actions/authAction";
+import { connect } from "react-redux";
 
 const Register = ({
   setAlert,
@@ -13,21 +13,21 @@ const Register = ({
 }) => {
   useEffect(() => {
     if (isAuthenticated) {
-      history.push('/');
+      history.push("/vehicles");
     }
 
-    if (error === 'User already exists') {
-      setAlert(error, 'danger');
+    if (error === "User already exists") {
+      setAlert(error, "danger");
       clearErrors();
     }
     // eslint-disable-next-line
   }, [error, isAuthenticated, history]); // want this to run when the error changes
 
   const [user, setUser] = useState({
-    name: '',
-    email: '',
-    password: '',
-    password2: ''
+    name: "",
+    email: "",
+    password: "",
+    password2: ""
   });
 
   const { name, email, password, password2 } = user;
@@ -39,51 +39,51 @@ const Register = ({
 
   const onSubmit = e => {
     e.preventDefault();
-    if (name === '' || email === '' || password === '') {
-      setAlert('Please enter all fields', 'danger');
+    if (name === "" || email === "" || password === "") {
+      setAlert("Please enter all fields", "danger");
     } else if (password !== password2) {
-      setAlert('Passwords do not match', 'danger');
+      setAlert("Passwords do not match", "danger");
     } else {
       register({ name, email, password });
     }
   };
 
   return (
-    <div className='form-container'>
+    <div className="form-container">
       <h1>
-        Account <span className='text-primary'>Register</span>
+        Account <span className="text-primary">Register</span>
       </h1>
       <form onSubmit={onSubmit}>
-        <div className='form-group'>
-          <label htmlFor='name'>Name</label>
-          <input type='text' name='name' value={name} onChange={onChange} />
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" value={name} onChange={onChange} />
         </div>
-        <div className='form-group'>
-          <label htmlFor='email'>Email Address</label>
-          <input type='email' name='email' value={email} onChange={onChange} />
+        <div className="form-group">
+          <label htmlFor="email">Email Address</label>
+          <input type="email" name="email" value={email} onChange={onChange} />
         </div>
-        <div className='form-group'>
-          <label htmlFor='password'>Password</label>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
           <input
-            type='password'
-            name='password'
+            type="password"
+            name="password"
             value={password}
             onChange={onChange}
           />
         </div>
-        <div className='form-group'>
-          <label htmlFor='password2'>Confirm password</label>
+        <div className="form-group">
+          <label htmlFor="password2">Confirm password</label>
           <input
-            type='password'
-            name='password2'
+            type="password"
+            name="password2"
             value={password2}
             onChange={onChange}
           />
         </div>
         <input
-          type='submit'
-          value='Register'
-          className='btn btn-primary btn-block'
+          type="submit"
+          value="Register"
+          className="btn btn-primary btn-block"
         />
       </form>
     </div>
@@ -95,7 +95,6 @@ const mapStateToProps = state => ({
   isAuthenticated: state.auth.isAuthenticated
 });
 
-export default connect(
-  mapStateToProps,
-  { setAlert, clearErrors, register }
-)(Register);
+export default connect(mapStateToProps, { setAlert, clearErrors, register })(
+  Register
+);
